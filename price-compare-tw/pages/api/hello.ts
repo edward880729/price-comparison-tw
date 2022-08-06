316df9454152d01508ee5e0f22c89c1b5794fa4a
edward880729 <edward.880729@gmail.com>
2022-08-06 21:18:42 +0800

    test shopee api

--- price-compare-tw/pages/api/hello.ts
+++ price-compare-tw/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextRequest } from 'next/server'
+import { WatchProduct, SearchResult } from '@prisma/client';
+import axios from 'axios';
 type Data = {
-  name: string
+  name: string | string[] | undefined
 }
 
-export default function handler(
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  let { keyword, } = req.query
+  keyword = encodeURI(keyword as string)
+  const response = await axios(`https://shopee.tw/api/v4/search/search_items?by=price&keyword=${keyword}&limit=100&newest=100&order=asc&page_type=search&price_max=800&price_min=50&scenario=PAGE_GLOBAL_SEARCH&skip_autocorrect=1&version=2`)
+  const result = await response.data
+  //res.status(response.status).json({ name: result.items.map((x: { item_basic: { name: any; }; }) => x.item_basic.name) })
+  res.status(response.status).json({ name: result.items.map((x: { item_basic: { name: string; }; }) => x.item_basic.name) })
 }
--- price-compare-tw/pages/index.tsx
+++ price-compare-tw/pages/index.tsx
@@ -4,35 +4,46 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import prisma from '../lib/prisma';
 import { WatchProduct, SearchResult } from '@prisma/client';
+import axios from 'axios';
+
+type Person = {
+  name: string[];
+};
 
 type Props = {
   //feed: WatchProduct[];
-  feed: SearchResult[];
+  //feed: SearchResult[];
+  feed: Person
 }; 
 
 const Home: React.FC<Props> = ({feed}) => {
   return (
     <div className={styles.container}>
-      {feed.map(x => (
-        <div>
-          <p>{x.title}</p>
-          <p>{x.price}</p>
-          <p>{x.createDate.toString()}</p>
-        </div>
-      ))}
+      {feed.name.filter(x => !x.includes("小霖")).map(x => (<p>{x}</p>))  }
     </div>
   )
 }
 
 
 export const getStaticProps: GetStaticProps = async () => {
+  if (typeof sessionStorage != "undefined" && sessionStorage.getItem("test"))
+    return {
+      props: { feed: JSON.parse(sessionStorage.getItem("test") as string) },
+    }
+  const response = await axios('http://localhost:3000/api/hello', {
+    params: {
+      keyword: 'ㄅㄆㄇ'
+    }
+  })
+  const result = await response.data
+
   const feed = await prisma.searchResult.findMany({
     where: {},
   });
   return {
-    props: { feed: JSON.parse(JSON.stringify(feed)) },
-    //revalidate: 10,
+    props: { feed: result },
   };
 };
+
 
 export default Home
