f18702866748d74204942f8a1bff8ac75b761aac
YUYING0327 <105634108+YUYING0327@users.noreply.github.com>
2022-08-26 10:17:11 +0800

    update search

--- price-compare-tw/components/SearchForm.tsx
+++ price-compare-tw/components/SearchForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface Params {
   website: string;
   keyword: string;
@@ -11,10 +13,17 @@ interface Props {
 }
 
 const SearchForm = ({ params, setParams }: Props) => {
-  console.log(params);
+  const [keyword, setKeyword] = useState('');
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setParams({ ...params, keyword });
+    setKeyword('');
+  };
+
   return (
     <div>
-      <form className="m-10">
+      <form className="m-10" onSubmit={handleSubmit}>
         <input
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setParams({ ...params, website: e.target.value });
@@ -26,9 +35,9 @@ const SearchForm = ({ params, setParams }: Props) => {
         />
         <input
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setParams({ ...params, keyword: e.target.value });
+            setKeyword(e.target.value);
           }}
-          defaultValue={params.keyword}
+          value={keyword}
           type="text"
           placeholder="Search..."
           className="outline-none border-2 rounded-md pl-2"
@@ -51,7 +60,7 @@ const SearchForm = ({ params, setParams }: Props) => {
           placeholder="max..."
           className="outline-none border-2 rounded-md pl-2"
         />
-        {/* <button onClick={search}>Search</button> */}
+        <button type="submit">Search</button>
       </form>
     </div>
   );
