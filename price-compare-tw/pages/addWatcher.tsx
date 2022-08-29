14963bb5d3db5c5825260fa67d7ba78098adf59b
YUYING0327 <105634108+YUYING0327@users.noreply.github.com>
2022-08-29 13:21:27 +0800

    update ssr axios params

--- price-compare-tw/pages/addWatcher.tsx
+++ price-compare-tw/pages/addWatcher.tsx
@@ -4,9 +4,6 @@ import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { useState } from 'react';
-import useSWR from 'swr';
-
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 type Props = {
   searchResult: SearchResult[];
@@ -34,42 +31,42 @@ const Home = ({ searchResult }: Props) => {
 
   return (
     <div>
-      <form className="m-10">
+      <form className='m-10'>
         <input
           onChange={(e) => setWebsite(e.target.value)}
           defaultValue={inputWebsite}
-          type="text"
-          placeholder="Website..."
-          className="outline-none border-2 rounded-md pl-2"
+          type='text'
+          placeholder='Website...'
+          className='outline-none border-2 rounded-md pl-2'
         />
         <input
           onChange={(e) => setKeyword(e.target.value)}
           defaultValue={inputKeyword}
-          type="text"
-          placeholder="Search..."
-          className="outline-none border-2 rounded-md pl-2"
+          type='text'
+          placeholder='Search...'
+          className='outline-none border-2 rounded-md pl-2'
         />
         <input
           onChange={(e) => setMinPrice(e.target.valueAsNumber)}
           defaultValue={inputMinPrice}
-          type="number"
-          placeholder="min..."
-          className="outline-none border-2 rounded-md pl-2"
+          type='number'
+          placeholder='min...'
+          className='outline-none border-2 rounded-md pl-2'
         />
         <input
           onChange={(e) => setMaxPrice(e.target.valueAsNumber)}
           defaultValue={inputMaxPrice}
-          type="number"
-          placeholder="max..."
-          className="outline-none border-2 rounded-md pl-2"
+          type='number'
+          placeholder='max...'
+          className='outline-none border-2 rounded-md pl-2'
         />
         <button onClick={search}>Search</button>
       </form>
-      <div className="grid gap-2 grid-cols-4">
+      <div className='grid gap-2 grid-cols-4'>
         {searchResult.map((data, index) => (
-          <div className="max-w-[300px] border rounded-md" key={index}>
+          <div className='max-w-[300px] border rounded-md' key={index}>
             <div>
-              <Image src={data.imageUrl} width={300} height={300} alt="" />
+              <Image src={data.imageUrl} width={300} height={300} alt='' />
             </div>
             <p>{data.name}</p>
             <p>{data.price}</p>
@@ -91,10 +88,10 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 
   const response = await axios('http://localhost:3000/api/search', {
     params: {
-      website: website,
-      keyword: keyword,
-      minPrice: minPrice,
-      maxPrice: maxPrice,
+      website,
+      keyword,
+      minPrice,
+      maxPrice,
     },
   });
   const result: SearchResult[] = await response.data;
