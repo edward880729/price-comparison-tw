0e0598f98d442220d3d9666b57ddbdf4bcd4c99e
edward880729 <edward.880729@gmail.com>
2022-09-17 15:42:09 +0800

    WatchProduct建構子更改

--- price-compare-tw/pages/api/insertWatchProduct.ts
+++ price-compare-tw/pages/api/insertWatchProduct.ts
@@ -5,24 +5,24 @@ export default async function insertWatchProduct(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ): Promise<void> {
-    let { website, keyword, minPrice, maxPrice } = req.query;
-    let result = "";
-    let statusCode;
+  let { website, keyword, minPrice, maxPrice } = req.query;
+  let result = "";
+  let statusCode;
   if (!keyword || keyword == '') {
-      statusCode = 400;
-      result = "條件請輸入完整";
+    statusCode = 400;
+    result = "條件請輸入完整";
   }
-    
-  const watchProduct = new WatchProduct(website as string, keyword as string, Number(minPrice), Number(maxPrice));
-  if (await watchProduct.isExistsInDB()) {
-      statusCode = 400;
-      result = "已有相同之尋找商品";
-    }
-  else {
-      watchProduct.insertOrUpdateToDB();
-      statusCode = 201;
-      result = "新增成功";
-    }
 
-    return res.status(statusCode).json(result);
+  const watchProduct = new WatchProduct(undefined, website as string, keyword as string, Number(minPrice), Number(maxPrice));
+  if (await watchProduct.isExistsInDB()) {
+    statusCode = 400;
+    result = "已有相同之尋找商品";
+  }
+  else {
+    watchProduct.insertOrUpdateToDB();
+    statusCode = 201;
+    result = "新增成功";
+  }
+
+  return res.status(statusCode).json(result);
 }
