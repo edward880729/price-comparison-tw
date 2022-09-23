ec336afae734dc332f0e6ae8f1a277f8331a890a
unknown <fxsh312088@gmail.com>
2022-09-23 22:04:35 +0800

    update ui

--- price-compare-tw/components/home/InsertProducts.tsx
+++ price-compare-tw/components/home/InsertProducts.tsx
@@ -22,15 +22,15 @@ function InsertProducts({ item }: Props) {
   const filterMinPrice = Math.min(...getAllPrice);
 
   return (
-    <div className='max-w-[224px] min-h-[224px] mb-2 flex items-center justify-center border-2 border-gray-500 rounded-md cursor-pointer hover:shadow-xl hover:scale-105 duration-300 insertProductBG'>
+    <div className='w-[224px] min-h-[224px] mb-2 flex items-center justify-center border-2 border-gray-500 rounded-md cursor-pointer hover:shadow-xl hover:scale-105 duration-300 insertProductBG'>
       <div className='flex flex-col justify-start h-full'>
-        <div className='relative border-b border-gray-500'>
+        <div className='relative border-gray-500'>
           <Image
             src={randomProduct?.imageUrl}
             alt={randomProduct?.name}
             width={224}
             height={130}
-            objectFit='contain'
+            objectFit='cover'
           />
           <p
             className={`absolute top-1 left-1 px-2 bg-white border rounded-md z-10 ${websiteTitle}`}
