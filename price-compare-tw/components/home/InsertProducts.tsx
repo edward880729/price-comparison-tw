61a08bc043cdea2386da283b225b2019389c4151
unknown <fxsh312088@gmail.com>
2022-09-23 22:35:32 +0800

    update bg

--- price-compare-tw/components/home/InsertProducts.tsx
+++ price-compare-tw/components/home/InsertProducts.tsx
@@ -21,17 +21,23 @@ function InsertProducts({ item }: Props) {
   const getAllPrice = item.searchResult.map((result) => result.price);
   const filterMinPrice = Math.min(...getAllPrice);
 
+  const styling = {
+    backgroundImage: `url('${randomProduct?.imageUrl}')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100%',
+  };
+
   return (
     <div className='w-[224px] min-h-[224px] mb-2 flex items-center justify-center border-2 border-gray-500 rounded-md cursor-pointer hover:shadow-xl hover:scale-105 duration-300 insertProductBG'>
-      <div className='flex flex-col justify-start h-full'>
-        <div className='relative border-gray-500'>
-          <Image
-            src={randomProduct?.imageUrl}
-            alt={randomProduct?.name}
-            width={224}
-            height={130}
-            objectFit='cover'
-          />
+      <div
+        className='relative flex flex-col justify-start h-full'
+        style={styling}
+      >
+        <div className='w-full h-full absolute insertProductBG z-0'></div>
+        <div className='relative border-gray-500 z-10'>
           <p
             className={`absolute top-1 left-1 px-2 bg-white border rounded-md z-10 ${websiteTitle}`}
           >
@@ -41,7 +47,7 @@ function InsertProducts({ item }: Props) {
             New
           </p>
         </div>
-        <div className='h-full flex flex-col justify-between'>
+        <div className='h-full flex flex-col justify-end z-10'>
           <p className='text-md mb-1'>關鍵字: {item?.keyword}</p>
           <p className='text-red-500'>最低價: {filterMinPrice}</p>
         </div>
